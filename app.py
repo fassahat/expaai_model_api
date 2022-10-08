13a098aecb91704f5a97a90274390fd1c891346e
fassahat <idfassahata@gmail.com>
2022-10-08 23:50:39 +0200

    using model

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, Response
 import json
 import time
+from transformers import pipeline
+
 app = Flask(__name__)
 
 @app.route('/hello', methods=['GET', 'POST'])
@@ -11,9 +13,22 @@ def welcome():
 def predict():
     data = request.get_json()
     sentences = data['sentences']
-    time.sleep(10) # we will call model somewhere here
-    result = { "advantages": [sentences[0]], "problems": [sentences[1]], "solutions": [sentences[2]] }
-    return Response(json.dumps(result),  mimetype='application/json')
+    # time.sleep(1) # we will call model somewhere here
+    # result = { "advantages": [sentences[11]], "problems": [sentences[13]], "solutions": [sentences[15]] }
+    # print(result)
+    classifier = pipeline("text-classification", "fassahat/distill-bert-finetuned-150k-patent-sentences", framework="tf")
+    result = classifier(sentences)
+    final_result = {'advantages':[], 'solutions':[], 'problems':[]}
+    for i in range(len(sentences)):
+        if (result[i]['score'] >= 0.8 and len(sentences[i]) > 10):
+            if (result[i]['label'] == '0'):
+                final_result['solutions'].append(sentences[i])
+            elif (result[i]['label'] == '1'):
+                final_result['advantages'].append(sentences[i])
+            else:
+                final_result['problems'].append(sentences[i])
+    
+    return Response(json.dumps(final_result),  mimetype='application/json')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=105)
