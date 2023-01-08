84805c1f5b25de0e7e672b46ec2af185cfd3e07b
fassahat <idfassahata@gmail.com>
2023-01-08 18:27:03 +0100

    testing models and using bert large

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ def predict():
     # print(result)
     classifier = pipeline("text-classification", "fassahat/anferico-bert-for-patents-finetuned-557k-patent-sentences", framework="tf")
     result = classifier(sentences)
+    print(result)
     final_result = {'advantages':[], 'solutions':[], 'problems':[]}
     for i in range(len(sentences)):
         if (result[i]['score'] >= 0.8 and len(sentences[i]) > 10):
@@ -36,10 +37,27 @@ def semantic_search():
     patent_text = data['patent_text']
     question = data['question']
 
-    question_answerer = pipeline("question-answering", model='deepset/roberta-base-squad2')
+    # question_answerer_roberta = pipeline("question-answering", model='deepset/roberta-base-squad2')
+    question_answerer_bert_large = pipeline("question-answering", model='bert-large-uncased-whole-word-masking-finetuned-squad')
+    # question_answerer_distilbert = pipeline("question-answering", model='distilbert-base-cased-distilled-squad')
 
-    result_model = question_answerer(question=question, context=patent_text)
-    result = { 'answer' : result_model['answer'] }
+    # answer_roberta = question_answerer_roberta(question=question, context=patent_text)
+    answer_bert_large = question_answerer_bert_large(question=question, context=patent_text)
+    # answer_distilbert = question_answerer_distilbert(question=question, context=patent_text)
+    # print('answer_roberta: ', answer_roberta)
+    print('answer_bert_large: ', answer_bert_large)
+    # print('answer_distilbert: ', answer_distilbert)
+
+    # result = {}
+    
+    # if ((answer_roberta['score'] >= answer_bert_large['score']) & (answer_roberta['score'] >= answer_distilbert['score'])):
+    #     result = { 'answer' : answer_roberta['answer'] }
+    # elif ((answer_bert_large['score'] >= answer_roberta['score']) & (answer_bert_large['score'] >= answer_distilbert['score'])):
+    #     result = { 'answer' : answer_bert_large['answer'] }
+    # else:
+    #     result = { 'answer' : answer_distilbert['answer'] }
+
+    result = { 'answer' : answer_bert_large['answer'] }
 
     return  Response(json.dumps(result),  mimetype='application/json')
 
